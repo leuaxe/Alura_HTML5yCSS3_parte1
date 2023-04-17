57bbcad2d4ef892aa4b69f0862144cb48f62deaf
leuaxe <leuaxe-488@hotmail.com>
2023-04-16 21:50:10 -0500

    borders y radios

--- encriptador.js
+++ encriptador.js
@@ -1,4 +1,4 @@
-var frase = "bienvenidos";
+var frase = "bien";
 var frase2 = "";
 
 /*e => enter
@@ -8,32 +8,5 @@ var frase2 = "";
   u -> ufat*/
 
 var inicio = 0;
+var final = 0;
 
-for (let index = 0; index < frase.length; index++) {
-    var letra = frase.charAt(index);
-    switch (letra) {
-        case 'a':
-            a.push(index);
-            break;
-        case 'e':
-            e.push(index);
-            break;
-        case 'i':
-            frase2 += frase.substring(inicio, index);
-            frase2 += "mes";
-            inicio = index;
-            break;
-        case 'o':
-            o.push(index);
-            break;
-        case 'u':
-            u.push(index);
-            break;
-    }
-}
-
-console.log(a);
-console.log(e);
-console.log(i);
-console.log(o);
-console.log(u);
